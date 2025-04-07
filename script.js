4966767402c8759be59d79f1a48d06cb60bbd571
cryptictwin <james-twin@live.nmit.ac.nz>
2025-04-08 09:43:13 +1200

    all functionality complete

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
+const readline = require('readline')
 
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+// Color codes to add color to the game
 const reset = "\x1b[0m"
 const red = "\x1b[91m"
 const blue = "\x1b[94m"
@@ -6,9 +12,20 @@ const green = "\x1b[92m"
 const cyan = "\x1b[96m"
 const player1 = `${red}X${reset}`
 const player2 = `${blue}O${reset}`
+
+// When set to true, the game will run. Else it will stop running
 let gameRunning = true
-let turnCount = Math.ceil(Math.random() * 4)
+
+// Gives a 50/50 chance of being either 1 or 2, providing
+// an equal chance for each player to make the first move
+let turnCount = Math.ceil(Math.random() * 2)
+
+// An array of empty strings for the gameboard, as players
+// make moves, the empty strings get replaced with either 'X' or 'O'
 let gameBoard = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+// An array of arrays. Each inner array represents three positions on the board that
+// equals a win state if the all contain the same player symbol
 const winStates = [
                 [0, 1, 2],
                 [3, 4, 5],
@@ -19,6 +36,7 @@ const winStates = [
                 [0, 4, 8],
                 [2, 4, 6]
 ]
+
 
 const printBoard = () => {
     console.log(
@@ -45,55 +63,80 @@ const playGame = () => {
         turnCount++
     } else if(gameBoard.indexOf(" ") === -1) {
         gameRunning = false
-        console.log(`${cyan}
-        Its a tie.
-        Play Again?
-        ${reset}`)
+        console.log(
+`${cyan}Its a tie.${reset}`)
+        rl.question('Play Again? (y/n)  ', input => {
+            input.trim().toLowerCase()
+            if(input === 'y' || input === "yes") {
+                gameBoard = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+                gameRunning = true
+                isGame();
+            } else {
+                rl.close()
+            }
+        })
     }
     else {
         return
     }
     
 }
-
-
-while(gameRunning) {
-    playGame()
-    let play1Board = [...gameBoard]
-    let play2Board = [...gameBoard]
-    let play1Array = []
-    let play2Array = []
-    while(play1Board.indexOf(player1) !== -1) {
-        let firstXIndex = play1Board.indexOf(player1)
-        play1Array.push(firstXIndex)
-        // Allows extraction of index of X, remove it but not update the length so
-        // to get the true index of the next X in the array
-        delete play1Board[firstXIndex]
-    }
-    while(play2Board.indexOf(player2) !== -1) {
-        let firstOIndex = play2Board.indexOf(player2)
-        play2Array.push(firstOIndex)
-        delete play2Board[firstOIndex]
-    }
-    if(play1Array.length >= 3 || play2Array.length >= 3){
-        for(let array of winStates) {
-            const winChecker = array.every(el => play1Array.includes(el))
-            const winChecker2 = array.every(el => play2Array.includes(el))
-            if(winChecker) {
-                gameRunning = false
-                console.log(`${green}
-        ${red}Player 1${reset} Wins!
-        Play Again?
-        ${reset}`)
-            } else if(winChecker2) {
-                gameRunning = false
-                console.log(`${green}
-        ${blue}Player 2${reset} Wins
-        Play Again?
-        ${reset}`)
-                
+function isGame() {
+    while(gameRunning) {
+        playGame()
+        let play1Board = [...gameBoard]
+        let play2Board = [...gameBoard]
+        let play1Array = []
+        let play2Array = []
+        while(play1Board.indexOf(player1) !== -1) {
+            let firstXIndex = play1Board.indexOf(player1)
+            play1Array.push(firstXIndex)
+            // Allows extraction of index of X, remove it but not update the length so
+            // to get the true index of the next X in the array
+            delete play1Board[firstXIndex]
+        }
+        while(play2Board.indexOf(player2) !== -1) {
+            let firstOIndex = play2Board.indexOf(player2)
+            play2Array.push(firstOIndex)
+            delete play2Board[firstOIndex]
+        }
+        if(play1Array.length >= 3 || play2Array.length >= 3){
+            for(let array of winStates) {
+                const winChecker = array.every(el => play1Array.includes(el))
+                const winChecker2 = array.every(el => play2Array.includes(el))
+                if(winChecker) {
+                    gameRunning = false
+                    console.log(`${green}
+${red}Player 1${reset} Wins!`)
+                    rl.question('Play Again? (y/n)  ', input => {
+                        input.trim().toLowerCase();
+                        if(input === 'y' || input === 'yes') {
+                            gameBoard = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+                            gameRunning = true
+                            isGame();
+                        } else {
+                            rl.close()
+                        }
+                    })
+                } else if(winChecker2) {
+                    gameRunning = false
+                    console.log(`${green}
+${blue}Player 2${reset} Wins`)
+                    rl.question('Play Again? (y/n)  ', input => {
+                        input.trim().toLowerCase()
+                        if(input === 'y' || input === 'yes') {
+                            gameBoard = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+                            gameRunning = true
+                            isGame();
+                        } else {
+                            rl.close()
+                        }
+                    })  
+                }
             }
         }
     }
 }
-    
+
+isGame();
+

2c92764e5692cf5380206076554106a604987518
cryptictwin <james-twin@live.nmit.ac.nz>
2025-04-08 12:55:59 +1200

    calculate win chance for going first

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const readline = require('readline')
+// const readline = require('readline')
 
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-})
+// const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+// })
 // Color codes to add color to the game
 const reset = "\x1b[0m"
 const red = "\x1b[91m"
@@ -12,6 +12,18 @@ const green = "\x1b[92m"
 const cyan = "\x1b[96m"
 const player1 = `${red}X${reset}`
 const player2 = `${blue}O${reset}`
+let p1StartP1WinCount = 0
+let p1StartP2WinCount = 0
+let p2StartP1WinCount = 0
+let p2StartP2WinCount = 0
+let play1IsFirst
+let play2IsFirst
+const p1StartWin = document.getElementById('play1StartWin')
+const p1StartLose = document.getElementById('play1StartLose')
+const p2StartWin = document.getElementById('play2StartWin')
+const p2StartLose = document.getElementById('play2StartLose')
+const restartButton = document.getElementById('restart')
+const chanceRate = document.getElementById('chanceOfWinning')
 
 // When set to true, the game will run. Else it will stop running
 let gameRunning = true
@@ -65,16 +77,16 @@ const playGame = () => {
         gameRunning = false
         console.log(
 `${cyan}Its a tie.${reset}`)
-        rl.question('Play Again? (y/n)  ', input => {
-            input.trim().toLowerCase()
-            if(input === 'y' || input === "yes") {
-                gameBoard = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-                gameRunning = true
-                isGame();
-            } else {
-                rl.close()
-            }
-        })
+        // rl.question('Play Again? (y/n)  ', input => {
+        //     input.trim().toLowerCase()
+        //     if(input === 'y' || input === "yes") {
+        //         gameBoard = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+        //         gameRunning = true
+        //         isGame();
+        //     } else {
+        //         rl.close()
+        //     }
+        // })
     }
     else {
         return
@@ -82,7 +94,20 @@ const playGame = () => {
     
 }
 function isGame() {
+    console.log(p1StartP1WinCount)
+    console.log(p1StartP2WinCount)
+    console.log(p2StartP1WinCount)
+    console.log(p2StartP2WinCount)
     while(gameRunning) {
+        if(gameBoard.every(el => el === " ")) {
+            if(turnCount % 2 === 0) {
+                play1IsFirst = true
+                play2IsFirst = false
+            } else if(turnCount % 2 === 1 || turnCount % 2 === 0.5) {
+                play1IsFirst = false
+                play2IsFirst = true
+            }
+        }
         playGame()
         let play1Board = [...gameBoard]
         let play2Board = [...gameBoard]
@@ -105,38 +130,64 @@ function isGame() {
                 const winChecker = array.every(el => play1Array.includes(el))
                 const winChecker2 = array.every(el => play2Array.includes(el))
                 if(winChecker) {
+                    if(play1IsFirst) {
+                        p1StartP1WinCount++
+                        p1StartWin.innerHTML = p1StartP1WinCount
+                    } else if(play2IsFirst) {
+                        p2StartP1WinCount++
+                        p2StartWin.innerHTML = p2StartP1WinCount
+                    }
+
                     gameRunning = false
                     console.log(`${green}
 ${red}Player 1${reset} Wins!`)
-                    rl.question('Play Again? (y/n)  ', input => {
-                        input.trim().toLowerCase();
-                        if(input === 'y' || input === 'yes') {
-                            gameBoard = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-                            gameRunning = true
-                            isGame();
-                        } else {
-                            rl.close()
-                        }
-                    })
+                    // rl.question('Play Again? (y/n)  ', input => {
+                    //     input.trim().toLowerCase();
+                    //     if(input === 'y' || input === 'yes') {
+                    //         gameBoard = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+                    //         gameRunning = true
+                    //         isGame();
+                    //     } else {
+                    //         rl.close()
+                    //     }
+                    // })
                 } else if(winChecker2) {
+                    if(play1IsFirst) {
+                        p1StartP2WinCount++
+                        p1StartLose.innerHTML = p1StartP2WinCount
+                    } else if(play2IsFirst) {
+                        p2StartP2WinCount++
+                        p2StartLose.innerHTML = p2StartP2WinCount
+                    }
                     gameRunning = false
                     console.log(`${green}
 ${blue}Player 2${reset} Wins`)
-                    rl.question('Play Again? (y/n)  ', input => {
-                        input.trim().toLowerCase()
-                        if(input === 'y' || input === 'yes') {
-                            gameBoard = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-                            gameRunning = true
-                            isGame();
-                        } else {
-                            rl.close()
-                        }
-                    })  
+                    // rl.question('Play Again? (y/n)  ', input => {
+                    //     input.trim().toLowerCase()
+                    //     if(input === 'y' || input === 'yes') {
+                    //         gameBoard = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+                    //         gameRunning = true
+                    //         isGame();
+                    //     } else {
+                    //         rl.close()
+                    //     }
+                    // })  
                 }
             }
         }
     }
+    chanceRate.innerHTML = 'By going first, your chance of winning is: %' + (((p1StartP1WinCount / (p1StartP1WinCount + p1StartP2WinCount)) * 100) + ((p2StartP2WinCount / (p2StartP1WinCount + p2StartP2WinCount)) * 100)) / 2
 }
 
-isGame();
+for(let i = 0; i < 1000; i++) {
+    gameBoard = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+    gameRunning = true
+    isGame()
+}
 
+
+restartButton.addEventListener('click', () => {
+    gameBoard = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+    gameRunning = true
+    isGame()
+})
